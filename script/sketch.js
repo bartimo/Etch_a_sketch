cca1631aa4022ef57800a263671aa0998d5ca1a3
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-14 17:59:14 -0500

    Add grid generation logic

--- script/sketch.js
+++ script/sketch.js
@@ -1,1 +1,14 @@
-console.log("test");
+const gridSize = 16;  //To be set with button later
+const container = document.querySelector('.container');
+
+
+for (i = 1; i <= gridSize; i++) {
+    let row = document.createElement('div');
+    row.className = 'gridRow';
+    container.appendChild(row);
+        for(j = 1; j <= gridSize; j++) {
+            let grid = document.createElement('div');
+            grid.className = 'grid';
+            row.append(grid);
+        }
+}
