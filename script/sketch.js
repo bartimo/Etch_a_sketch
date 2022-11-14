06c6307dae4f7b50f121f57813ad7279688d34f0
Bart Zweigoron <bzweigoron@gmail.com>
2022-11-14 18:04:26 -0500

    change grid creation to function

--- script/sketch.js
+++ script/sketch.js
@@ -1,14 +1,23 @@
+//Variable Decelerations
 const gridSize = 16;  //To be set with button later
 const container = document.querySelector('.container');
 
 
-for (i = 1; i <= gridSize; i++) {
-    let row = document.createElement('div');
-    row.className = 'gridRow';
-    container.appendChild(row);
-        for(j = 1; j <= gridSize; j++) {
-            let grid = document.createElement('div');
-            grid.className = 'grid';
-            row.append(grid);
-        }
+createGrid(gridSize)
+
+
+
+//Create an even grid of the provided size.
+function createGrid(size) {
+    for (i = 1; i <= gridSize; i++) {
+        let row = document.createElement('div');
+        row.className = 'gridRow';
+        container.appendChild(row);
+            for(j = 1; j <= gridSize; j++) {
+                let grid = document.createElement('div');
+                grid.id = j + "-" + i;
+                grid.className = 'grid';
+                row.append(grid);
+            }
+    }
 }
